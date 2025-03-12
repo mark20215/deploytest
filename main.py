926c8362185d3f47789a452a92ae19a7fad2b99e
mark20215 <112085362+mark20215@users.noreply.github.com>
2025-03-12 23:06:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-from typing import Optional
+import pickle
 
+import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+with open("composer.pkl", "rb") as f:
+    preprocessor = pickle.load(f)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class Item(BaseModel):
+    name: str
+    year: int
+    selling_price: int
+    km_driven: int
+    fuel: str
+    seller_type: str
+    transmission: str
+    owner: str
+    mileage: str
+    engine: str
+    max_power: str
+    torque: str
+    seats: float
+
+
+class Items(BaseModel):
+    objects: List[Item]
+
+indexator = 1
+
+@app.post("/predict_item")
+def predict_item(item: Item) -> float:
+    global indexator
+    dictionary = item.model_dump()
+    df = pd.DataFrame(dictionary, index=[indexator])
+    df = df.drop('selling_price', axis=1)
+    indexator += 1
+    df = preprocessor.transform(df)
+    return model.predict(df)
+
+@app.post("/predict_items")
+def predict_items(items: List[Item]) -> List[float]:
+    output = []
+    for item in items:
+        output.append(predict_item(item))
+    return output
